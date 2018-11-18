1e9be91110d9feb7a8dc3e94e40b103b88974b9b
ishaandhamija <ishaan.dhamija@gmail.com>
2018-11-18 17:48:45 +0530

    shared pref added

--- app/src/main/java/com/example/shivam/aapat/Activities/DashboardActivity.java
+++ app/src/main/java/com/example/shivam/aapat/Activities/DashboardActivity.java
@@ -1,8 +1,10 @@
 package com.example.shivam.aapat.Activities;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -10,12 +12,25 @@ import com.example.shivam.aapat.R;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    SharedPreferences sharedpreferences;
+    public static final String MyPREFERENCES = "MyPrefs";
+
     LinearLayout profileLayout,nearbyLayout,tipsLayout,requestLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString("my_userId", "-LRa4gDBjG9HjTyar1pF");
+        editor.putString("my_name", "Ishaan");
+        editor.putString("my_number", "9958421789");
+        editor.putString("my_blood_group", "O+");
+        editor.putBoolean("my_donation_willingness", true);
+        editor.commit();
 
         profileLayout = (LinearLayout) findViewById(R.id.profileLayout);
         nearbyLayout = (LinearLayout) findViewById(R.id.nearbyLayout);
